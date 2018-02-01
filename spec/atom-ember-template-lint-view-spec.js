7e1d23a14d364a16ff66d4f653dad5a851a6659a
rakesh.gr <rakesh.gr@zohocorp.com>
2018-02-02 00:21:20 +0530

    working version--still differs from linter v2 docs

--- spec/atom-ember-template-lint-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomEmberTemplateLintView from '../lib/atom-ember-template-lint-view';
-
-describe('AtomEmberTemplateLintView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
